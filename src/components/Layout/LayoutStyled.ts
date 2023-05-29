1cd5b8bf8815b98f5f5d4e943ed46c58c4cfb8dd
Marc Moreno <marcmoreno112@gmail.com>
2023-05-29 17:06:28 +0200

    Create Layout Styled

---
+++ src/components/Layout/LayoutStyled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 30px;
+  gap: 2rem;
+
+  h1 {
+    color: ${(props) => props.theme.colors.primary};
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+`;
+
+export default LayoutStyled;

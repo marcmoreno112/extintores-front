c66bddd49c102d5020becb1667ab6a9d069b6f0b
Marc Moreno <marcmoreno112@gmail.com>
2023-05-29 15:23:58 +0200

    Create theme

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#c31e24",
+    secondary: "#573928",
+    tertiary: "#F1D0C7",
+    "feedback-ok": "23AD29",
+  },
+
+  fonts: {
+    primary: "'Archivo', sans-serif",
+    secondary: "sans-serif",
+  },
+};
+
+export default theme;

42d9376072fb4558e4c74eb901a873531b71c0b2
Marc Moreno <marcmoreno112@gmail.com>
2023-05-29 14:28:21 +0200

    Install husky and commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   formatter: "@commitlint/format",
   rules: {
-    "header-min-length": [2, "always", 10],
+    "header-min-length": [2, "always", 11],
     "header-max-length": [2, "always", 72],
     "header-case": [2, "always", ["sentence-case"]],
     "type-empty": [2, "always"],

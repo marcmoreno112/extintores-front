fc21d7aff4f6ed880840faacffa84b38497057b2
Marc Moreno <marcmoreno112@gmail.com>
2023-05-29 15:24:45 +0200

    Create global styles

---
+++ src/styles/GlobalStyles/GlobalStyles.ts
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*, ::before, ::after {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: ${(props) => props.theme.fonts.primary};
+  font-size: 1rem;
+}
+
+body {
+  margin: 30px;
+  font-family: inherit;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: inherit;
+  font-size: 1.2rem;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+  margin-bottom: 0;
+}
+
+input {
+  font-family: inherit;
+  border: none;
+}
+
+button {
+  cursor: pointer;
+  font: inherit;
+  border: none;
+  background-color: transparent;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+p {
+  margin: 0
+}
+
+img {
+  max-width: 100%
+}
+`;
+
+export default GlobalStyle;

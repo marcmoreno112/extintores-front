22b2061f0e628a2ccfc4e9e44e990dd971ee9e3d
Marc Moreno <marcmoreno112@gmail.com>
2023-05-30 09:27:01 +0200

    Use renderWithProviders at header test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,38 +1,13 @@
-import {
-  RouteObject,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
 import Header from "./Header";
-import { render, screen } from "@testing-library/react";
-import { ThemeProvider } from "styled-components";
-import theme from "../../styles/theme/theme";
-import GlobalStyle from "../../styles/GlobalStyles/GlobalStyles";
-import { Provider } from "react-redux";
-import { store } from "../../store";
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../utils/testUtils";
 
 describe("Given a Header component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an image with the alternative text 'Extintores'", () => {
       const expectedTitle = "Extintores";
 
-      const routes: RouteObject[] = [
-        {
-          path: "/",
-          element: <Header />,
-        },
-      ];
-
-      const mockRouter = createBrowserRouter(routes);
-
-      render(
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Provider store={store}>
-            <RouterProvider router={mockRouter} />
-          </Provider>
-        </ThemeProvider>
-      );
+      renderWithProviders(<Header />);
 
       const title = screen.getByRole("heading", { name: expectedTitle });
 

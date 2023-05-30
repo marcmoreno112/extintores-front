23ddeaf7544a0ec9b411f7335e9fbe8f2492d1d0
Marc Moreno <marcmoreno112@gmail.com>
2023-05-30 11:59:04 +0200

    Create LoginFormStyled

--- src/components/Header/HeaderStyled.ts
+++ src/components/Header/HeaderStyled.ts
@@ -4,7 +4,10 @@ const HeaderStyled = styled.header`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   gap: 50px;
+  width: 100%;
+  position: fixed;
 
   h1 {
     color: ${(props) => props.theme.colors.primary};
---
+++ src/components/LoginForm/LoginFormStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const LoginFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+export default LoginFormStyled;

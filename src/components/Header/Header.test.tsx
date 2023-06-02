0594c38d305daebee5fb9818085c631dd85f901d
Marc Moreno <marcmoreno112@gmail.com>
2023-06-02 08:44:02 +0200

    Test logout button shown when there is a token

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,16 @@
 import Header from "./Header";
 import { screen } from "@testing-library/react";
-import { renderWithProvidersRouter } from "../../utils/testUtils";
+import {
+  renderWithProviders,
+  renderWithProvidersRouter,
+} from "../../utils/testUtils";
+import { tokenMock } from "../../mocks/userMocks";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import App from "../App/App";
 
 describe("Given a Header component", () => {
   describe("When it is rendered", () => {
@@ -12,6 +22,32 @@ describe("Given a Header component", () => {
       const title = screen.getByRole("heading", { name: expectedTitle });
 
       expect(title).toBeInTheDocument();
+    });
+  });
+
+  describe("When there is a token in localStorage", () => {
+    test("Then it should show a 'Logout' button", async () => {
+      const token = tokenMock;
+      const expectedButtonText = "Logout";
+
+      localStorage.setItem("token", token);
+
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <App />,
+        },
+      ];
+
+      const mockRouter = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={mockRouter} />);
+
+      const button = await screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      expect(button).toBeInTheDocument();
     });
   });
 });

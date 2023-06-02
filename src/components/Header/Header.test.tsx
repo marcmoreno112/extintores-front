b90a797103a5ba6505188799a341a0936a7de611
Marc Moreno <marcmoreno112@gmail.com>
2023-06-02 10:01:50 +0200

    Test logout removes the token at localStorage and shows the login button

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,16 +1,6 @@
 import Header from "./Header";
 import { screen } from "@testing-library/react";
-import {
-  renderWithProviders,
-  renderWithProvidersRouter,
-} from "../../utils/testUtils";
-import { tokenMock } from "../../mocks/userMocks";
-import {
-  RouteObject,
-  RouterProvider,
-  createMemoryRouter,
-} from "react-router-dom";
-import App from "../App/App";
+import { renderWithProvidersRouter } from "../../utils/testUtils";
 
 describe("Given a Header component", () => {
   describe("When it is rendered", () => {
@@ -22,32 +12,6 @@ describe("Given a Header component", () => {
       const title = screen.getByRole("heading", { name: expectedTitle });
 
       expect(title).toBeInTheDocument();
-    });
-  });
-
-  describe("When there is a token in localStorage", () => {
-    test("Then it should show a 'Logout' button", async () => {
-      const token = tokenMock;
-      const expectedButtonText = "Logout";
-
-      localStorage.setItem("token", token);
-
-      const routes: RouteObject[] = [
-        {
-          path: "/",
-          element: <App />,
-        },
-      ];
-
-      const mockRouter = createMemoryRouter(routes);
-
-      renderWithProviders(<RouterProvider router={mockRouter} />);
-
-      const button = await screen.getByRole("button", {
-        name: expectedButtonText,
-      });
-
-      expect(button).toBeInTheDocument();
     });
   });
 });
--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,6 +1,18 @@
 import { screen } from "@testing-library/react";
 import Navigation from "./Navigation";
-import { renderWithProvidersRouter } from "../../utils/testUtils";
+import {
+  renderWithProviders,
+  renderWithProvidersRouter,
+} from "../../utils/testUtils";
+import { vi } from "vitest";
+import { tokenMock } from "../../mocks/userMocks";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import App from "../App/App";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Navigation component", () => {
   describe("When it is rendered", () => {
@@ -8,8 +20,11 @@ describe("Given a Navigation component", () => {
       const expectedListText = "Lista";
       const expectedLoginText = "Login";
       const expectedSumText = "Create element";
+      const onClickFunction = vi.fn();
 
-      renderWithProvidersRouter(<Navigation />);
+      renderWithProvidersRouter(
+        <Navigation onClickFunction={onClickFunction} />
+      );
 
       const lista = screen.getByRole("link", { name: expectedListText });
       const login = screen.getByRole("link", { name: expectedLoginText });
@@ -18,6 +33,95 @@ describe("Given a Navigation component", () => {
       expect(lista).toBeInTheDocument();
       expect(login).toBeInTheDocument();
       expect(sum).toBeInTheDocument();
+    });
+  });
+
+  describe("When there is a token in localStorage", () => {
+    test("Then it should show a 'Logout' button", async () => {
+      const token = tokenMock;
+      const expectedButtonText = "Logout";
+
+      localStorage.setItem("token", token);
+
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <App />,
+        },
+      ];
+
+      const mockRouter = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={mockRouter} />);
+
+      const button = await screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user clicks the 'logout' button", () => {
+    test("Then the token key in localStorage shouldn't exist", async () => {
+      const token = tokenMock;
+      const expectedButtonText = "Logout";
+
+      localStorage.setItem("token", token);
+
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <App />,
+        },
+      ];
+
+      const mockRouter = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={mockRouter} />);
+
+      const button = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      await userEvent.click(button);
+
+      const resultToken = localStorage.getItem("token");
+
+      expect(resultToken).toBeNull();
+    });
+
+    test("Then Navigation component should show the 'Login' link and not show the 'Logout' link", async () => {
+      const token = tokenMock;
+      const expectedButtonText = "Logout";
+
+      localStorage.setItem("token", token);
+
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <App />,
+        },
+      ];
+
+      const mockRouter = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={mockRouter} />);
+
+      const button = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      await userEvent.click(button);
+
+      const resultButtonText = "Login";
+
+      const newButton = await screen.getByRole("link", {
+        name: resultButtonText,
+      });
+
+      expect(button).not.toBeInTheDocument();
+      expect(newButton).toBeInTheDocument();
     });
   });
 });

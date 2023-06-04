a7a3fffd3804d66e72ca947eab6e90f66793c54b
Marc Moreno <marcmoreno112@gmail.com>
2023-06-04 14:14:23 +0200

    Create ExtinguishersListStyled component

---
+++ src/components/ExtinguishersList/ExtinguishersListStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ExtinguishersListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+export default ExtinguishersListStyled;

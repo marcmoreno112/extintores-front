946ba4d7885fa63af780906178302878d22fae54
Marc Moreno <marcmoreno112@gmail.com>
2023-06-04 16:05:55 +0200

    Create ExtinguisherCardStyled component

---
+++ src/components/ExtinguisherCard/ExtinguisherCardStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ExtinguisherCardStyled = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+export default ExtinguisherCardStyled;

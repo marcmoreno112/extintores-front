6970a1bbaa4ca0a98e92109c955b16e3ff81a911
Marc Moreno <marcmoreno112@gmail.com>
2023-06-06 17:02:16 +0200

    Create NotFoundPageStyled component

---
+++ src/pages/NotFoundPage/NotFoundPageStyled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const NotFoundPageStyled = styled.main`
+  width: 100%;
+  padding-top: 180px;
+
+  .page-title {
+    font-size: 1.8rem;
+    padding-bottom: 50px;
+  }
+`;
+
+export default NotFoundPageStyled;

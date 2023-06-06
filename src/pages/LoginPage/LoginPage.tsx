1c47cab4a05f0ac60fbc12d2b90e3f01a32dd8f2
Marc Moreno <marcmoreno112@gmail.com>
2023-06-06 16:29:03 +0200

    Move Loader component to App

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,12 +2,11 @@ import { useNavigate } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import useToken from "../../hooks/useToken/useToken";
 import useUser from "../../hooks/useUser/useUser";
-import { useAppDispatch, useAppSelector } from "../../store";
+import { useAppDispatch } from "../../store";
 import { loginUserActionCreator } from "../../store/user/userSlice";
 import { UserStructure } from "../../types";
 import LoginPageStyled from "./LoginPageStyled";
 import useLocalStorage from "../../hooks/useLocalStorage/useLocalStorage";
-import Loader from "../../components/Loader/Loader";
 
 const LoginPage = (): React.ReactElement => {
   const { getToken } = useUser();
@@ -15,7 +14,6 @@ const LoginPage = (): React.ReactElement => {
   const { decodeToken } = useToken();
   const navigate = useNavigate();
   const { addToLocalStorage } = useLocalStorage();
-  const { isLoading } = useAppSelector((state) => state.uiState);
 
   const onSubmitLoginUser = async (userCredentials: UserStructure) => {
     const token = await getToken(userCredentials);
@@ -35,7 +33,6 @@ const LoginPage = (): React.ReactElement => {
 
   return (
     <LoginPageStyled>
-      {isLoading ? <Loader /> : ""}
       <h2 className="page-title">Login</h2>
       <LoginForm submitFunction={onSubmitLoginUser} />
     </LoginPageStyled>

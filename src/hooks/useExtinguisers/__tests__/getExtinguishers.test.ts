de5e4e99f7f526d3603a6f81f736f9c1280492b1
Marc Moreno <marcmoreno112@gmail.com>
2023-06-06 10:53:01 +0200

    Test getExtinguishers function throwing an error

--- src/hooks/useExtinguisers/__tests__/getExtinguishers.test.ts
+++ src/hooks/useExtinguisers/__tests__/getExtinguishers.test.ts
@@ -2,6 +2,8 @@ import { renderHook } from "@testing-library/react";
 import useExtinguishers from "../useExtinguishers";
 import { extinguishersMock } from "../../../mocks/extinguishersMocks";
 import { providerWrapper } from "../../../utils/testUtils";
+import { server } from "../../../mocks/server";
+import { errorHandlers } from "../../../mocks/handlers";
 
 describe("Given a getExtinguishers function", () => {
   describe("When it is called", () => {
@@ -17,6 +19,23 @@ describe("Given a getExtinguishers function", () => {
       const expectedExtinguishers = extinguishersMock;
 
       expect(extinguishers).toStrictEqual(expectedExtinguishers);
+    });
+  });
+  describe("When it is called and there is an error in database", () => {
+    test("Then it should throw a 'Database problem'", () => {
+      server.resetHandlers(...errorHandlers);
+
+      const expectedError = new Error("Database problem");
+
+      const {
+        result: {
+          current: { getExtinguishers },
+        },
+      } = renderHook(() => useExtinguishers(), { wrapper: providerWrapper });
+
+      const extinguishers = getExtinguishers();
+
+      expect(extinguishers).rejects.toThrowError(expectedError);
     });
   });
 });

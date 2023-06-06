9515a4555a717fc5838c719f26701e6732cd785c
Marc Moreno <marcmoreno112@gmail.com>
2023-06-06 14:36:47 +0200

    Test close button at modal

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,6 +1,6 @@
-import { renderWithProviders } from "../../utils/testUtils";
+import { providerWrapper, renderWithProviders } from "../../utils/testUtils";
 import App from "./App";
-import { screen } from "@testing-library/react";
+import { renderHook, screen } from "@testing-library/react";
 import { initialUserState } from "../../store/user/userSlice";
 import { UserStateStructure } from "../../store/user/types";
 import {
@@ -9,6 +9,10 @@ import {
   createMemoryRouter,
 } from "react-router-dom";
 import { tokenMock } from "../../mocks/userMocks";
+import modalErrors from "../Modal/modalErrors";
+import { UiState } from "../../store/ui/types";
+import userEvent from "@testing-library/user-event";
+import useExtinguishers from "../../hooks/useExtinguisers/useExtinguishers";
 
 describe("Given an App component", () => {
   describe("When it is rendered", () => {
@@ -58,6 +62,75 @@ describe("Given an App component", () => {
       });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is called and there is an error getting extintores", () => {
+    test(`Then it should show a '${modalErrors.getItemsError.text}' modal`, () => {
+      const preloadedState: UiState = {
+        isLoading: false,
+        hasModal: true,
+        modal: modalErrors.getItemsError,
+      };
+
+      const expectedText = modalErrors.getItemsError.text;
+
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <App />,
+        },
+      ];
+
+      const mockRouter = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={mockRouter} />, {
+        uiState: preloadedState,
+      });
+
+      const modal = screen.getByAltText(expectedText);
+
+      expect(modal).toBeInTheDocument();
+    });
+    describe("And the user clicks the close button", () => {
+      test("Then the modal should disappear", async () => {
+        const preloadedState: UiState = {
+          isLoading: false,
+          hasModal: true,
+          modal: modalErrors.getItemsError,
+        };
+
+        const expectedText = modalErrors.getItemsError.text;
+
+        const routes: RouteObject[] = [
+          {
+            path: "/",
+            element: <App />,
+          },
+        ];
+
+        const mockRouter = createMemoryRouter(routes);
+
+        renderWithProviders(<RouterProvider router={mockRouter} />, {
+          uiState: preloadedState,
+        });
+
+        const {
+          result: {
+            current: { getExtinguishers },
+          },
+        } = renderHook(() => useExtinguishers(), { wrapper: providerWrapper });
+
+        await getExtinguishers();
+
+        const modal = screen.getByAltText(expectedText);
+        const expectedButtonAlt = "close button";
+        const button = screen.getByAltText(expectedButtonAlt);
+
+        await userEvent.click(button);
+
+        expect(modal).not.toBeInTheDocument();
+      });
     });
   });
 });

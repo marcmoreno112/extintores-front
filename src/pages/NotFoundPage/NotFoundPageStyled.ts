4d0f9295c341d3b15c17288832f702d5aa290480
Marc Moreno <marcmoreno112@gmail.com>
2023-06-06 17:10:54 +0200

    Add NotFoundPage to appRouter

--- src/pages/NotFoundPage/NotFoundPageStyled.ts
+++ src/pages/NotFoundPage/NotFoundPageStyled.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const NotFoundPageStyled = styled.main`
   width: 100%;
   padding-top: 180px;
+  text-align: center;
 
   .page-title {
     font-size: 1.8rem;

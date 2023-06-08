a92007f12db08aaa079fa4806d40adaa64570339
Marc Moreno <marcmoreno112@gmail.com>
2023-06-08 22:38:13 +0200

    Create FormStyled component

---
+++ src/components/Form/FormStyled.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 30px;
+
+  .form {
+    &__text-field {
+      padding: 18px;
+      background-color: ${(props) => props.theme.colors.tertiaryDark};
+      border-radius: 8px;
+      color: white;
+      font-size: 1.2rem;
+    }
+
+    &__control {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+    }
+
+    &__send-button {
+      background-color: ${(props) => props.theme.colors.tertiary};
+      font-size: 1.2rem;
+      font-weight: 700;
+      padding: 18px;
+      border-radius: 8px;
+      width: 120px;
+    }
+
+    &__send-button-container {
+      display: flex;
+      justify-content: right;
+    }
+
+    &__label {
+      font-size: 1.3rem;
+    }
+  }
+`;
+
+export default FormStyled;

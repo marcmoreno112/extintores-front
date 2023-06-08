cd40a101804f9278e3a7efa767bbdcb27085aced
Marc Moreno <marcmoreno112@gmail.com>
2023-06-08 21:12:31 +0200

    Create AddExtinguisherPage

---
+++ src/pages/AddExtinguisherPage/AddExtinguisherPage.tsx
@@ -0,0 +1,11 @@
+import AddExtinguisherPageStyled from "./AddExtinguisherPageStyled";
+
+const AddExtinguisherPage = (): React.ReactElement => {
+  return (
+    <AddExtinguisherPageStyled>
+      <h2 className="page-title">Add extinguisher</h2>
+    </AddExtinguisherPageStyled>
+  );
+};
+
+export default AddExtinguisherPage;

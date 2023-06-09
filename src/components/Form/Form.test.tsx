6daac15a0384b4fa1749d03638ab6a4de6c79418
Marc Moreno <marcmoreno112@gmail.com>
2023-06-09 11:37:56 +0200

    Test form state

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../utils/testUtils";
 import Form from "./Form";
@@ -38,6 +39,41 @@ describe("Given a Form component", () => {
       const button = screen.getByRole("button", { name: expectedButtonText });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user types 'Hello' in the 'Marca' input", () => {
+    test("Then it should show the text 'Hello' inside of the input", async () => {
+      renderWithProviders(<Form userId="" buttonText={expectedButtonText} />);
+
+      const brandInputText = "Marca";
+
+      const brandInput = screen.getByLabelText(brandInputText);
+
+      const typedText = "Hello";
+
+      await userEvent.type(brandInput, typedText);
+
+      expect(brandInput).toHaveValue(typedText);
+    });
+  });
+
+  describe("When it is rendered and the user clicks the 'Clase A' checkbox", () => {
+    test("Then it should show the checkbox checked", async () => {
+      renderWithProviders(<Form userId="" buttonText={expectedButtonText} />);
+
+      const aClassCheckboxText = "Clase A";
+
+      const aClassCheckbox = screen.getByLabelText(aClassCheckboxText);
+
+      await userEvent.click(aClassCheckbox);
+
+      const checkedCheckbox = screen.getByRole("checkbox", {
+        name: aClassCheckboxText,
+        checked: true,
+      });
+
+      expect(checkedCheckbox).toBeInTheDocument();
     });
   });
 });

3ad59c63eda1ac454f663d18a8b342e2b14cc783
Marc Moreno <marcmoreno112@gmail.com>
2023-06-10 17:41:54 +0200

    Test loadMoreExtinguishers reducer

---
+++ src/store/extinguishers/__tests__/loadMoreExtinguishers.test.ts
@@ -0,0 +1,30 @@
+import {
+  extinguishersReducer,
+  loadMoreExtinguishersActionCreator,
+} from "../extinguishersSlice";
+import { ExtinguishersStateStructure } from "../types";
+
+describe("Given a loadMoreExtinguishers reducer", () => {
+  describe("When it receives an initialExtinguishersState", () => {
+    test("Then it should return a new state with the loadNumber property increased by 1", () => {
+      const initialExtinguishersState: ExtinguishersStateStructure = {
+        extinguishers: [],
+        loadNumber: 1,
+      };
+
+      const expectedExtinguishersState: ExtinguishersStateStructure = {
+        extinguishers: [],
+        loadNumber: 2,
+      };
+
+      const loadMoreExtinguishersAction = loadMoreExtinguishersActionCreator();
+
+      const newExtinguishersState = extinguishersReducer(
+        initialExtinguishersState,
+        loadMoreExtinguishersAction
+      );
+
+      expect(newExtinguishersState).toStrictEqual(expectedExtinguishersState);
+    });
+  });
+});

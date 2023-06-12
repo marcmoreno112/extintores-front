58a2321a954f0b835c89d17e7ae312d43aed4849
Marc Moreno <marcmoreno112@gmail.com>
2023-06-12 10:22:57 +0200

    Create NoExtinguishersStyled component

---
+++ src/components/NoExtinguishersComponent/NoExtinguishersStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const NoExtinguishersStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export default NoExtinguishersStyled;

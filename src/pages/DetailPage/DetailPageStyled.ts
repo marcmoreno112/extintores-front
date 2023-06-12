3fa6deb91d2f27816ade7957afeb71e127d32929
Marc Moreno <marcmoreno112@gmail.com>
2023-06-12 21:06:38 +0200

    Create DetailPageStyled component

---
+++ src/pages/DetailPage/DetailPageStyled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const DetailPageStyled = styled.main`
+  width: 100%;
+  padding-top: 130px;
+
+  .page-title {
+    font-size: 1.8rem;
+  }
+`;
+
+export default DetailPageStyled;
